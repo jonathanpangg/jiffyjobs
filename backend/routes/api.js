4d71f8734860a329dfd4977984829b26841328e1
Lucas <junsuny789@gmail.com>
2023-10-02 15:02:50 -0400

    backend

---
+++ backend/routes/api.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/todos', (req, res, next) => {
+  // get placeholder
+});
+
+router.post('/todos', (req, res, next) => {
+  // post placeholder
+});
+
+router.delete('/todos/:id', (req, res, next) => {
+  // delete placeholder
+});
+
+module.exports = router;
