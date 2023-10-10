8ca6b1b5e42b8b118720fcd42a143442e178216e
Jonathan Pang <pangj@bu.edu>
2023-10-10 16:44:42 -0400

    set up dummy data

--- frontend/jiffyjobs/src/App.js
+++ frontend/jiffyjobs/src/App.js
@@ -4,8 +4,8 @@ import { JobTable } from './components/JobTable'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div /*className="App"*/>
+      <header /*className="App-header"*/>
         <JobTable> </JobTable>
       </header>
     </div>
--- frontend/jiffyjobs/src/components/JobTable.js
+++ frontend/jiffyjobs/src/components/JobTable.js
@@ -3,66 +3,21 @@ import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: 'firstName',
-    headerName: 'First name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'lastName',
-    headerName: 'Last name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 110,
-    editable: true,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
+    { field: 'id', headerName: 'ID', width: 25 },
+    { field: 'jobName', headerName: 'Job Name', width: 125 },
+    { field: 'startDate', headerName: 'Start Date', width: 125 },
+    { field: 'endDate', headerName: 'End Date', width: 125 },
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, jobName: "Building Table", startDate: "11/28/2023", endDate: "12/1/2023" }, 
+    { id: 2, jobName: "Tutoring", startDate: "10/5/2023", endDate: "10/7/2023" },
 ];
 
 export function JobTable() {
-  return (
-    <Box sx={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: {
-              pageSize: 5,
-            },
-          },
-        }}
-        pageSizeOptions={[5]}
-        checkboxSelection
-        disableRowSelectionOnClick
-      />
-    </Box>
-  );
+    return (
+        <Box sx={{ height: 400, width: '100%' }}>
+        <DataGrid rows={rows} columns={columns}/>
+        </Box>
+    );
 }
