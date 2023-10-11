08be354c2c2ece68f12757677fcb788a61a327a0
“Calvin <Calvinli0824@gmail.com>
2023-10-11 15:22:20 -0400

    finished job controllers

--- backend/controllers/job.js
+++ backend/controllers/job.js
@@ -28,7 +28,7 @@ export const deleteJobsByID = async (req, res) => {
     try {
         const deleteJob = await Jobs.findByIdAndRemove(id);
 
-		if (!job) {
+		if (!deleteJob) {
 			return handleNotFound(res, "Job not found");
 		}
 
@@ -53,11 +53,37 @@ export const postJobs = async (req, res) => {
             date_posted
         });
 
-        if (!job) {
+        if (!makeJob) {
             return handleBadRequest(res, "Unable to create Job");
         }
 
         return handleSuccess(res, makeJob);
+    } catch (error) {
+        return handleServerError(res, "Internal server error");
+    }
+};
+
+export const updateJobs = async (req, res) => {
+    const { title, description, pay, location, categories, time, date_posted} = req.body;
+    const { id } = req.params;
+
+    try {
+
+        const editJob = await Jobs.findByIdAndUpdate({
+            title,
+            description,
+            pay,
+            location,
+            categories,
+            time,
+            date_posted
+        });
+
+        if (!editJob) {
+            return handleBadRequest(res, "Unable to edit Job");
+        }
+
+        return handleSuccess(res, editJob);
     } catch (error) {
         return handleServerError(res, "Internal server error");
     }
