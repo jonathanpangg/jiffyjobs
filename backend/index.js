e9277b261a3f5b0da75ab361e023632a43472e1d
Lucas <junsuny789@gmail.com>
2023-10-11 17:17:13 -0400

    adding routes

--- backend/index.js
+++ backend/index.js
@@ -2,22 +2,27 @@ require('dotenv').config()
 
 const express = require('express');
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 
 // express app
 const app = express();
 
-const port = process.env.PORT || 5000;
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(cors({
+  origin: '*'
+}));
 
+// middleware
+app.use(express.json())
 app.use((req, res, next) => {
-  res.send('Welcome to Express');
-});
+  console.log(req.path, req.method)
+  next()
+})
+
+
+// routes
+// app.use()
 
 
 // connect to db
