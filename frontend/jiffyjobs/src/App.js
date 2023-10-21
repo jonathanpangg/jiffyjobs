f327221e8e8d6d9e641bbaa77bc1f033a24732cd
Annie Liang <aaliang@bu.edu>
2023-10-21 00:55:13 -0400

    creating dropdown of filters

--- frontend/jiffyjobs/src/App.js
+++ frontend/jiffyjobs/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { NavBar } from './components/NavBar'
 import { JobBoard } from './components/JobBoard'
+import { FilterSection } from './components/FilterSelection'
+import { Filter, CheckboxDropdown} from './components/Filter'
 import { Routes, Route } from "react-router-dom";
 
 function App() {
--- frontend/jiffyjobs/src/components/JobBoard.js
+++ frontend/jiffyjobs/src/components/JobBoard.js
@@ -10,8 +10,31 @@ import ClearIcon from '@mui/icons-material/Clear';
 import IconButton from '@mui/material/IconButton';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
 
 export function JobBoard() {
+
+    const [flocation, setfLocation] = useState([]);
+    const [category, setCategory] = useState([]);
+    const [duration, setDuration] = useState([]);
+    const [payRate, setPayRate] = useState([]);
+    const [onOffCampus, setOnOffCampus] = useState([]);
+
+
+    const handleLocationeChange = (event) => { setfLocation(event.target.value); };
+    const handleCategoryChange = (event) => { setCategory(event.target.value); };
+    const handleDurationChange = (event) => { setDuration(event.target.value); };
+    const handlePayRateChange = (event) => { setPayRate(event.target.value); };
+    const handleOnOffCampusChange = (event) => { setOnOffCampus(event.target.value); };
+  
+    const locationOptions = ['Less than a mile away', '1-2 miles away', '3-5 miles away', '7+ miles away'];
+    const categoryOptions = ['Cleaning', 'Food/Restaurant', 'Office jobs', 'Retail', 'Other'];
+    const durationOptions = ['Less than 6 hours', '1 day', 'Few days - 1 week', 'A few weeks', '1 month', 'More than 1 month'];
+    const payOptions = ['$15/hour', '$15-20/hour', '$20+/hour', 'Stipend based'];
+    const campusOptions = ['On campus', 'Off campus'];
+
+
     const [jobData, setJobData] = useState([])
     const [openStartPop, setOpenStartPop] = useState(false)
     const [openSecondPop, setOpenSecondPop] = useState(false)
@@ -313,6 +336,160 @@ export function JobBoard() {
                         
                         <text style={{width: "100%"}} className='recently-posted-jobs'> 
                             Recently Posted Jobs
+
+                            <Grid container  flexWrap="nowrap" style={{ paddingRight: '15%', paddingTop: '1.5%'}}>
+                                    <Grid item xs={1} sm={6}>
+                                        <FormControl sx={{ m: 1, width: 200}}>
+                                            <InputLabel>Location</InputLabel>
+                                            <Select
+                                                label="Location"
+                                                multiple
+                                                value={flocation}
+                                                onChange={handleLocationeChange}
+                                                renderValue={(selected) => (
+                                                    <div>
+                                                    {selected.map((value) => (
+                                                        <div key={value}>{value}</div>
+                                                    ))}
+                                                    </div>
+                                                )}
+                                                >
+                                                {locationOptions.map((option) => (
+                                                    <MenuItem key={option} value={option}>
+                                                    <div>
+                                                        <input
+                                                        type="checkbox"
+                                                        checked={flocation.indexOf(option) > -1}
+                                                        />
+                                                        {option}
+                                                    </div>
+                                                    </MenuItem>
+                                                ))}
+                                            </Select>
+                                        </FormControl>
+                                    </Grid>
+                                    <Grid item xs={12} sm={6}>
+                                    <FormControl sx={{ m: 1, width: 200}}>
+                                            <InputLabel>Category</InputLabel>
+                                            <Select
+                                                label="Category"
+                                                multiple
+                                                value={category}
+                                                onChange={handleCategoryChange}
+                                                renderValue={(selected) => (
+                                                    <div>
+                                                    {selected.map((value) => (
+                                                        <div key={value}>{value}</div>
+                                                    ))}
+                                                    </div>
+                                                )}
+                                                >
+                                                {categoryOptions.map((option) => (
+                                                    <MenuItem key={option} value={option}>
+                                                    <div>
+                                                        <input
+                                                        type="checkbox"
+                                                        checked={category.indexOf(option) > -1}
+                                                        />
+                                                        {option}
+                                                    </div>
+                                                    </MenuItem>
+                                                ))}
+                                            </Select>
+                                        </FormControl>
+                                    </Grid>
+                                    <Grid item xs={12} sm={6}>
+                                    <FormControl sx={{ m: 1, width: 200}}>
+                                            <InputLabel>Duration</InputLabel>
+                                            <Select
+                                                label="Duration"
+                                                multiple
+                                                value={duration}
+                                                onChange={handleDurationChange}
+                                                renderValue={(selected) => (
+                                                    <div>
+                                                    {selected.map((value) => (
+                                                        <div key={value}>{value}</div>
+                                                    ))}
+                                                    </div>
+                                                )}
+                                                >
+                                                {durationOptions.map((option) => (
+                                                    <MenuItem key={option} value={option}>
+                                                    <div>
+                                                        <input
+                                                        type="checkbox"
+                                                        checked={duration.indexOf(option) > -1}
+                                                        />
+                                                        {option}
+                                                    </div>
+                                                    </MenuItem>
+                                                ))}
+                                            </Select>
+                                        </FormControl>
+                                    </Grid>
+                                    <Grid item xs={12} sm={6}>
+                                    <FormControl sx={{ m: 1, width: 200}}>
+                                            <InputLabel>Pay Rate</InputLabel>
+                                            <Select
+                                                label="Pay Rate"
+                                                multiple
+                                                value={payRate}
+                                                onChange={handlePayRateChange}
+                                                renderValue={(selected) => (
+                                                    <div>
+                                                    {selected.map((value) => (
+                                                        <div key={value}>{value}</div>
+                                                    ))}
+                                                    </div>
+                                                )}
+                                                >
+                                                {payOptions.map((option) => (
+                                                    <MenuItem key={option} value={option}>
+                                                    <div>
+                                                        <input
+                                                        type="checkbox"
+                                                        checked={payRate.indexOf(option) > -1}
+                                                        />
+                                                        {option}
+                                                    </div>
+                                                    </MenuItem>
+                                                ))}
+                                            </Select>
+                                        </FormControl>
+                                    </Grid>
+                                    <Grid item xs={12} sm={6}>
+                                    <FormControl sx={{ m: 1, width: 200}}>
+                                            <InputLabel>On/Off Campus</InputLabel>
+                                            <Select
+                                                label="On/Off Campus"
+                                                multiple
+                                                value={onOffCampus}
+                                                onChange={handleOnOffCampusChange}
+                                                renderValue={(selected) => (
+                                                    <div>
+                                                    {selected.map((value) => (
+                                                        <div key={value}>{value}</div>
+                                                    ))}
+                                                    </div>
+                                                )}
+                                                >
+                                                {campusOptions.map((option) => (
+                                                    <MenuItem key={option} value={option}>
+                                                    <div>
+                                                        <input
+                                                        type="checkbox"
+                                                        checked={onOffCampus.indexOf(option) > -1}
+                                                        />
+                                                        {option}
+                                                    </div>
+                                                    </MenuItem>
+                                                ))}
+                                            </Select>
+                                        </FormControl>
+                                    </Grid>     
+                                </Grid>
+                            
                         </text> 
                         {jobData.map((key) => (
                             <Grid key={key} item>
