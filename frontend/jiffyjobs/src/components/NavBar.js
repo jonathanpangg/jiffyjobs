eea6dc644e5209f88f7a5142e9e44d0371033c0b
Annie Liang <aaliang@bu.edu>
2023-11-03 00:38:41 -0400

    updated css for nav bar

--- frontend/jiffyjobs/src/components/NavBar.js
+++ frontend/jiffyjobs/src/components/NavBar.js
@@ -8,27 +8,43 @@ export function NavBar() {
         navigate('/JobBoard')
     }
 
+    const goToDashboard = () => {
+        navigate('/dashboard');
+    };
+
     return (
         <div className='nav-outer'> 
              <h1 className='logo-font'>
                 JIFFYJOBS
             </h1>
+            <div className='tab-font'></div>
+            <div className='tab-font'></div>
+            <div className='tab-font'></div>
+            <div className='tab-font'></div>
+            <div className='tab-font'></div>
+            <div className='tab-font'></div>
+
             <div className='first-tab' onClick={AllJobs}>
                 All Jobs 
             </div>
-            <div className='tab-font'>
+            <div className='tab-font' onClick={goToDashboard}>
+                Dashboard
+            </div>
+            
+            <div className='name-font' style={{ display: 'flex', alignItems: 'center' }}>
+                <div className='profile-picture'></div>
+                <span style={{ fontWeight: 'bold' }}>Lucas Yoon</span>
+            </div>
+            {/* <div className='tab-font'>
                 Recent Jobs
-            </div>
-            <div className='tab-font'>
+            </div> */}
+            {/* <div className='tab-font'>
                 Progress
-            </div>
-            <div className='tab-font'>
+            </div> */}
+            {/* <div className='tab-font'>
                 Notifications
-            </div>
+            </div> */}
             {/* <Divider orientation="vertical" flexItem /> */}
-            <div className='name-font'>
-                Lucas Yoon
-            </div>
         </div>
     )
 }
