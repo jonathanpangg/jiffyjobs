2b33301b350bde27bb2281c2e1ea4f598d1101c9
“Calvin <Calvinli0824@gmail.com>
2023-11-05 01:54:55 -0400

    pop up css all finished

--- frontend/jiffyjobs/src/pages/JobBoard.js
+++ frontend/jiffyjobs/src/pages/JobBoard.js
@@ -13,6 +13,7 @@ import ClearIcon from '@mui/icons-material/Clear';
 import IconButton from '@mui/material/IconButton';
 
 
+
 export function JobBoard() {
     const [jobData, setJobData] = useState([])
     const [rawData, setRawData] = useState([]);
@@ -37,7 +38,7 @@ export function JobBoard() {
                     setRawData(data);
                     const newJobData = data.map(function(obj) {
                         console.log(obj.time)
-                        return [[0, obj.title], ["", "Job Provider: " + obj.job_poster], ["", obj.location], ["", "Pay: $" + obj.pay], ["", "Description: " + obj.description], ["", "Time: " + dayjs(new Date(obj.time[0])).format('MM/DD/YY h:mm A')  + " " + " - " + dayjs(new Date(obj.time[1])).format('h:mm A')], ["", "Categories: " + obj.categories.toString()]]
+                        return [[0, obj.title], ["", obj.job_poster], ["", obj.location], ["", obj.pay], ["", obj.description], ["", dayjs(new Date(obj.time[0])).format('MM/DD/YY h:mm A')  + " " + " - " + dayjs(new Date(obj.time[1])).format('h:mm A')], ["", obj.categories.toString()]]
                     });
                     setJobData(newJobData);
 
@@ -80,7 +81,7 @@ export function JobBoard() {
                 .then((data) => {
                     setRawData(data);
                     const newJobData = data.map(function(obj) {
-                        return [[0, obj.title], ["", "Job Provider: " + obj.job_poster], ["", obj.location], ["", "Pay: $" + obj.pay], ["", "Description: " + obj.description], ["", "Time: " + dayjs(new Date(obj.time[0])).format('MM/DD/YY h:mm A')  + " " + " - " + dayjs(new Date(obj.time[1])).format('h:mm A')], ["", "Categories: " + obj.categories.toString()]]
+                        return [[0, obj.title], ["", obj.job_poster], ["", obj.location], ["", obj.pay], ["", obj.description], ["", dayjs(new Date(obj.time[0])).format('MM/DD/YY h:mm A')  + " " + " - " + dayjs(new Date(obj.time[1])).format('h:mm A')], ["", obj.categories.toString()]]
                     });
                     setJobData(newJobData);
                     setSize(jobData.length)
@@ -131,42 +132,88 @@ export function JobBoard() {
     return (
         <div className={'job-board-outer' + background}>
             <Dialog open={openPop} onClose={closePop} maxWidth={"1000px"} PaperProps={{sx: { borderRadius: "15px"}}}>
-                <div className='popup-title'>
-                    <DialogTitle style={{width: "90%"}}> 
-                    {currentPop[0] && currentPop[0].length > 1 && currentPop[0][1]}
-                    </DialogTitle>
-                    <IconButton onClick={closePop}>
+                
+                
+                <img
+                    style={{ maxWidth: '100%', maxHeight: '30vh', position: 'relative'}}
+                    src="https://source.unsplash.com/random"
+                    alt="placeholder"
+                />
+                    <IconButton onClick={closePop} style={{position: 'absolute', paddingLeft: '97%'}}>
                         <ClearIcon/>
                     </IconButton>
-                </div>
-                <Divider/>
+                
                 <DialogContent>
                     <DialogContentText ref={descriptionElementRefStartPop} tabIndex={-1} style={{width: '1000px'}}>
                         <div>
-                            <text>{currentPop[1] && currentPop[1].length > 1 && currentPop[1][1]}</text>
+                            <Typography style={{fontFamily: 'outline', fontSize:'32px', color:'#000', fontWeight:'600'}}>
+                                {currentPop[0] && currentPop[0].length > 1 && currentPop[0][1]}
+                            </Typography>
+                            <Typography style={{fontFamily: 'outline', fontSize:'24px', color:'#141414', fontWeight: '500'}}>
+                                {currentPop[1] && currentPop[1].length > 1 && currentPop[1][1]}
+                            </Typography>
                         </div>
                         <div style={{paddingTop: '2.5%'}}>
-                            <text>{currentPop[2] && currentPop[2].length > 1 && currentPop[2][1]}</text>
+                            <Typography style={{fontFamily: 'outline', fontSize: '20px', color: '#141414', fontWeight: '600'}}>
+                                Job Information
+                            </Typography>
                         </div>
-                        <div style={{paddingTop: '2.5%'}}>
-                            <text>{currentPop[3] && currentPop[3].length > 1 && currentPop[3][1]}</text>
+                        <div style={{paddingTop: '2.5%', paddingLeft:'2.5%'}}>
+                            <div style={{display: 'inline-block', width: '98px'}}>
+                                <Typography style={{fontFamily: 'outline', fontSize:'18px', color:'#5B5B5B', fontWeight:'400'}}>
+                                    Pay
+                                </Typography>
+                                <Typography style={{fontFamily: 'outline', fontSize:'18px', color:'#5B5B5B', fontWeight:'400'}}>
+                                    Location
+                                </Typography>
+                                <Typography style={{fontFamily: 'outline', fontSize:'18px', color:'#5B5B5B', fontWeight:'400'}}>
+                                    Time
+                                </Typography>
+                            </div>
+                            <div style={{display: 'inline-block'}}>
+                                <Typography style={{fontFamily:'outline', fontSize: '18px', color:'#141414', fontWeight: '600'}}>
+                                    ${currentPop[3] && currentPop[3].length > 1 && currentPop[3][1]}
+                                </Typography>
+                                <Typography style={{fontFamily:'outline', fontSize: '18px', color:'#141414', fontWeight: '600'}}>
+                                    <u>{currentPop[2] && currentPop[2].length > 1 && currentPop[2][1]}</u>
+                                </Typography>
+                                <Typography style={{fontFamily:'outline', fontSize: '18px', color:'#141414', fontWeight: '600'}}>
+                                    {currentPop[5] && currentPop[5].length > 1 && currentPop[5][1]}
+                                </Typography>
+                            </div>
                         </div>
+                        <Divider style={{ width: '100%', borderBottomWidth: '2px'}}/>
                         <div style={{paddingTop: '2.5%'}}>
-                            <text>{currentPop[4] && currentPop[4].length > 1 && currentPop[4][1]}</text>
+                            <Typography style={{fontFamily: 'outline', fontSize: '20px', color:'#141414', fontWeight: '600'}}>
+                                Job Description
+                            </Typography>
                         </div>
-                        <div style={{paddingTop: '2.5%'}}>
-                            <text>{currentPop[5] && currentPop[5].length > 1 && currentPop[5][1]}</text>
+                        <div style={{padding: '2.5%'}}>
+                            <Typography style={{fontFamily: 'outline', fontSize: '18px', color:'#5B5B5B', fontWeight: '400'}}>
+                                {currentPop[4] && currentPop[4].length > 1 && currentPop[4][1]}
+                            </Typography>
                         </div>
                         <div style={{paddingTop: '2.5%'}}>
                             <text>{currentPop[6] && currentPop[6].length > 1 && currentPop[6][1]}</text>
                         </div>
                     </DialogContentText>
                 </DialogContent>
-                <Divider/>
-                    <DialogActions>
-                        <Card sx={{height: 50, width: '10%'}} style={{overflow:'hidden', borderRadius: '15px', background: "#4169E1", color: 'white'}}>
+                <Divider style={{borderBottomWidth: '2px'}}/>
+                    <DialogActions style={{ justifyContent: 'center' }}>
+                        <Card sx={{height: 50, width: '30%'}} style={{overflow:'hidden', borderRadius: '15px', background: "#D9D9D9", color: 'white'}}>
                             <CardContent style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}} onClick={() => console.log("applied")}> 
-                                Apply
+                                <div>
+                                    <div style={{display:'inline-block'}}>
+                                    <Typography style={{fontFamily: 'Outfit', fontSize:'19.2px', color:'#5B5B5B', fontWeight:'400'}}>
+                                        Easy Apply with 
+                                    </Typography>
+                                    </div>
+                                    <div style={{display:'inline-block', paddingLeft: '5px'}}>
+                                    <Typography style={{fontFamily: 'Righteous-Regular', fontSize:'19.2px', color:'#141414', fontWeight:'400'}}>
+                                         JIFFYJOBS
+                                    </Typography>
+                                    </div>
+                                </div>
                             </CardContent>
                         </Card>
                     </DialogActions>
@@ -203,20 +250,20 @@ export function JobBoard() {
                                             image="https://source.unsplash.com/random"
                                             
                                         />
-                                        <Typography style={{fontFamily: 'Outfit'}} fontSize="14px" paddingLeft='10px' paddingRight='10px' paddingTop='10px'>
+                                        <Typography style={{fontFamily: 'Outfit', fontSize:"14px", paddingLeft:'10px', paddingRight:'10px', paddingTop:'10px'}}>
                                             <u>{key[0][1]}</u>
                                         </Typography>
-                                        <Typography style={{fontFamily: 'Outfit'}} fontSize="12px" paddingLeft='10px' paddingTop='15px'>
-                                            {key[3][1]}
+                                        <Typography style={{fontFamily: 'Outfit', fontSize:"12px", paddingLeft:'10px', paddingRight:'10px', paddingTop:'15px'}}>
+                                            Pay: ${key[3][1]}
                                         </Typography>
-                                        <Typography style={{fontFamily: 'Outfit'}} fontSize="12px" paddingLeft='10px' paddingRight='10px'>
+                                        <Typography style={{fontFamily: 'Outfit', fontSize:"12px", paddingLeft:'10px', paddingRight:'10px'}}>
                                             Location: <u>{key[2][1]}</u>
                                         </Typography>
-                                        <Typography style={{fontFamily: 'Outfit'}} fontSize="12px" paddingLeft='10px' paddingRight='10px'>
-                                            {key[5][1]}
+                                        <Typography style={{fontFamily: 'Outfit', fontSize:"12px", paddingLeft:'10px', paddingRight:'10px'}}>
+                                            Time: {key[5][1]}
                                         </Typography>
-                                        <Typography style={{fontFamily: 'Outfit'}} fontSize="12px" paddingLeft='10px' paddingRight='10px' paddingTop='10px' paddingBottom='10px'>
-                                            {truncate(key[4][1])}
+                                        <Typography style={{fontFamily: 'Outfit', fontSize:"12px", paddingLeft:'10px', paddingRight:'10px', paddingTop:'10px', paddingBottom:'10px'}}>
+                                            Description: {truncate(key[4][1])}
                                         </Typography>
                                     </Card>
                                 </Link>
