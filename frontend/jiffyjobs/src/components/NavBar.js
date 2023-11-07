6e7d529300987f30f6b9a11b214d9945d3bceade
Annie Liang <aaliang@bu.edu>
2023-11-07 12:51:30 -0500

    fix nav bar dropdown placement

--- frontend/jiffyjobs/src/components/NavBar.js
+++ frontend/jiffyjobs/src/components/NavBar.js
@@ -1,27 +1,38 @@
 import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
-// import { Divider } from '@mui/material';
-import Popover from '@mui/material/Popover';
 import MenuItem from '@mui/material/MenuItem';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Tooltip from '@mui/material/Tooltip';
+import Grid from '@mui/material/Grid';
 
 export function NavBar() {
     const navigate = useNavigate()
     const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
 
-    const handlePopoverOpen = (event) => {
-        setAnchorEl(event.currentTarget);
-      };
-    
-      const handlePopoverClose = () => {
-        setAnchorEl(null);
-      };
+    const settings = ['Profile', 'Dashboard', 'Setting', 'Logout'];
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+    };
+
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
 
     const AllJobs = () => {
         navigate('/JobBoard')
     }
-
-
 
     const Profile = () => {
         navigate('/Profile')
@@ -40,56 +51,63 @@ export function NavBar() {
 
     }
 
+    const settingsActions = {
+        'Profile': Profile,
+        'Dashboard': goToDashboard,
+        'Setting': handleSettings,
+        'Logout': handleLogout
+    };
+
     return (
-        <div className='nav-outer'> 
+        <Grid container className="nav-outer">
              <h1 className='logo-font' onClick={AllJobs}>
                 JIFFYJOBS
             </h1>
             <div className='tab-font'></div>
             <div className='tab-font'></div>
-
-
-            
+            <div className='tab-font'></div>
+        
 
             <div className='first-tab' onClick={AllJobs}>
                 All Jobs 
             </div>
 
-
-            <div className='tab-font' onClick={goToDashboard}>
-                Dashboard
-            </div>
-
             
-            <div className='name-font' style={{ display: 'flex', alignItems: 'center' }} aria-owns={open ? 'mouse-over-popover' : undefined} aria-haspopup="true" onMouseEnter={handlePopoverOpen} onMouseLeave={handlePopoverClose}>
-                <div className='profile-picture'></div>
-                <span style={{ fontWeight: 'bold' }}>Lucas Yoon</span>
-                <Popover
-                     open={Boolean(anchorEl)}
-                     anchorEl={anchorEl}
-                     onClose={handlePopoverClose}
-                     anchorOrigin={{
-                       vertical: 'bottom',
-                       horizontal: 'right',
-                     }}
-                    >
-                    <MenuItem onClick={Profile}>Profile</MenuItem>
-                    <MenuItem onClick={handleSettings}>Settings</MenuItem>
-                    <MenuItem onClick={handleLogout}>Logout</MenuItem>
-                </Popover>
+            <div className='name-font' style={{ display: 'flex', alignItems: 'center' }}   >
+
+                <Tooltip style={{ display: 'flex', alignItems: 'center'}} onClick={handleOpenUserMenu}>
+                    <div className='profile-picture'></div>
+                    <span style={{ fontWeight: 'bold' }} >Lucas Yoon</span>
+                </Tooltip>
+            
+                <Menu
+                    sx={{ mt: '45px' }}
+                    id="menu-appbar"
+                    anchorEl={anchorElUser}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    open={Boolean(anchorElUser)}
+                    onClose={handleCloseUserMenu}
+                >
+                    {settings.map((setting) => (
+                        <MenuItem 
+                            key={setting} 
+                            onClick={() => {
+                                handleCloseUserMenu(); 
+                                settingsActions[setting](); 
+                            }}>
+                            <Typography textAlign="center">{setting}</Typography>
+                        </MenuItem>
+                    ))}
+                </Menu>
             </div>
-
-            {/* <div className='tab-font'>
-                Recent Jobs
-            </div> */}
-            {/* <div className='tab-font'>
-                Progress
-            </div> */}
-            {/* <div className='tab-font'>
-                Notifications
-            </div> */}
-            {/* <Divider orientation="vertical" flexItem /> */}
-
-        </div>
+        </Grid>
     )
 }
