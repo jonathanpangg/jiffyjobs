83ac3dbd1e77f42ab07832a9c6ceb88d1fd1e536
“Calvin <Calvinli0824@gmail.com>
2023-11-11 23:44:00 -0500

    updated routes to reflect previous change

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,8 @@
 import express from "express";
 import { 
     seekerSignUp, 
-    seekerLogin, 
+    Login, 
     providerSignUp,
-    providerLogin
 } from "../controllers/authController.js";
 
 const router = express.Router();
@@ -11,12 +10,9 @@ const router = express.Router();
     // route for seekers to sign up
     router.post("/seekerSignUp", seekerSignUp);
 
-    //route for seekers to login
-    router.post("/seekerLogin", seekerLogin);
+    //route for login
+    router.post("/Login", Login);
 
     //route for providers to sign up
     router.post("/providerSignUp", providerSignUp);
-
-    //route for providers to login
-    router.post("/providerLogin", providerLogin);
 export default router;
