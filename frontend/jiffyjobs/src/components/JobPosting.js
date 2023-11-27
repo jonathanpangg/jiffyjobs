bece843a18a7cab4e77b043527637b9d35e90beb
Annie Liang <aaliang@bu.edu>
2023-11-27 01:30:00 -0500

    fixed pay bug of it can be empty and still cont

--- frontend/jiffyjobs/src/components/JobPosting.js
+++ frontend/jiffyjobs/src/components/JobPosting.js
@@ -194,14 +194,14 @@ export function JobPosting() {
     }
 
     const openNextPop = () => {
-        if (val.title === '' || val.name === '' || val.location === '' || val.pay === 0) {
-            handleError()
+        if (val.title === '' || val.name === '' || val.location === '' || val.pay === '' || parseFloat(val.pay) <= 0) {
+            handleError();
         } else {
             setError({
                 titleError: val.title === '',
                 nameError: val.name === '',
                 locationError: val.location === '',
-                valError: val.pay === '' || val.pay === 0,
+                valError: false,
                 descriptionError: false,
                 categoryError: false,
             })
