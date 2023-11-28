af141d3673040ad742b673f5b6b328da358f73e6
Annie Liang <aaliang@bu.edu>
2023-11-28 12:02:16 -0500

    congrats popup for submitted profile

--- frontend/jiffyjobs/src/pages/JobBoard.js
+++ frontend/jiffyjobs/src/pages/JobBoard.js
@@ -374,16 +374,21 @@ export function JobBoard() {
             onClose(); 
         };
         return (
-            <Dialog open={open} onClose={onClose} maxWidth={"1000px"} PaperProps={{sx: { borderRadius: "15px"}}}>
-                <DialogTitle>Congratulations!</DialogTitle>
-                <DialogContent>
-                    <Typography>Your profile has been successfully submitted.</Typography>
+            <Dialog open={open} onClose={onClose} maxWidth={"500px"} PaperProps={{ sx: { width: '345px', height: '220px', borderRadius: "15px", display: 'flex', flexDirection: 'column', alignItems: 'center', fontFamily: 'Outfit' } }}>
+                <DialogTitle style={{ textAlign: 'center', fontFamily: 'Outfit' }}>Congratulations!</DialogTitle>
+                <DialogContent style={{ textAlign: 'center', fontFamily: 'Outfit' }}>
+                    <Typography style={{ fontSize: '17px' }}>You have successfully submitted your profile. You can track your status in the Dashboard.</Typography>
                 </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleApplyMore}>Apply More</Button>
-                    <Button onClick={handleToDashboard}>Go to Dashboard</Button>
+                <Divider style={{ width: '100%', height: '4px', paddingTop: '14px' }} />
+                <DialogActions style={{ justifyContent: 'center' }}>
+                    <Button onClick={handleToDashboard} sx={{ border: '1px solid #5B5B5B', borderRadius: '8px', padding: '6px 12px', textTransform: 'none', color: '#5B5B5B', margin: '0 8px' }}>
+                        View Dashboard
+                    </Button>
+                    <Button onClick={handleApplyMore} sx={{ border: '1px solid #D9D9D9', borderRadius: '8px', padding: '6px 12px', textTransform: 'none', color: '#5B5B5B', backgroundColor: '#D9D9D9', '&:hover': {backgroundColor: '#D9D9D9'}, margin: '0 8px' }}>
+                        Back to Job Board
+                    </Button>
                 </DialogActions>
-            </Dialog>
+            </Dialog>        
         );
     }
 
