bc2623bbe527ccb0374119c0edd09167b674321e
Annie Liang <aaliang@bu.edu>
2023-11-29 14:38:36 -0500

    fix conflicts from cssfixing to searchbar

--- frontend/jiffyjobs/src/pages/JobBoard.js
+++ frontend/jiffyjobs/src/pages/JobBoard.js
@@ -15,23 +15,9 @@ import dayjs from 'dayjs';
 import { Filter } from '../components/Filter';
 import { Sort } from '../components/Sort';
 import { JobPosting } from '../components/JobPosting';
-<<<<<<< HEAD
-import dayjs from 'dayjs';
-import ClearIcon from '@mui/icons-material/Clear';
-import IconButton from '@mui/material/IconButton';
-import Chip from '@mui/material/Chip';
-import TextField from '@mui/material/TextField';
-import { useNavigate } from 'react-router-dom';
-import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
-import StarIcon from '@mui/icons-material/Star';
-import StarBorderIcon from '@mui/icons-material/StarBorder';
-import { ToastContainer, toast } from 'react-toastify';
-import JobCards from '../components/JobCards';
-=======
 import { JobCards } from '../components/JobCards';
 import { CongratsPopup } from '../components/CongratsPopup';
->>>>>>> 4002052259aa3f1df789f2d3305e862388312ac0
+
 
 
 export function JobBoard() {
@@ -61,10 +47,23 @@ export function JobBoard() {
 
     const navigate = useNavigate();
 
-    // goes to dashboard
+
     const handleToDashboard = () => {
         navigate('/dashboard');
     };
+  
+    function processTime(time) {
+        var str = "Time: "
+        for (let i = 0; i < time.length; i++) {
+            if (i%2 === 0) {
+                str = str + dayjs(new Date(time[i])).format('MM/DD/YY h:mm A') + " - "
+            } else {
+                str = str + dayjs(new Date(time[i])).format('h:mm A') + "\n"
+            }
+        }
+
+        return str
+    }
 
     const randomImage = (seed) => {
         return `https://source.unsplash.com/random?${seed}`;
@@ -172,24 +171,20 @@ export function JobBoard() {
 
     }, [filterList])
 
-    // handles truncating job description
     function truncate(str) {
         return str.length > 80 ? str.substring(0, 77) + "..." : str;
     }
 
-    // handles opening job listing popup
     const closePop = () => {
         setOpenPop(false);
     }
     
-    // handles opening job listing popup
     const openPopUp = (key) => {
         setCurrentPop(key);
         console.log(currentPop);
         setOpenPop(true);
     }
 
-    // handles description element ref
     const descriptionElementRefStartPop = React.useRef(null)
     useEffect(() => {
         if (openPopUp) {
@@ -200,7 +195,6 @@ export function JobBoard() {
         }
     }, [openPopUp])
 
-    // handles logging job data
     function handleLogJobData() {
         console.log('Data', jobData)
         console.log('Raw', rawData)
@@ -269,6 +263,10 @@ export function JobBoard() {
         setOpenSubmitProfile(false);
     };
 
+    // const handleOpeningSubmitProfile = () => {
+    //     setOpenSubmitProfile(true);
+    // };
+
     // submit profile popup
     function SubmitProfilePopup({ open, onClose, onSubmit }) {
         return (
@@ -334,7 +332,6 @@ export function JobBoard() {
         );
     }
 
-    // handles submitting profile
     const handleSubmitProfile = () => {
         handleCloseSubmitProfile();
         setOpenCongratsPopup(true);
@@ -389,6 +386,31 @@ export function JobBoard() {
 
     };
 
+    function CongratsPopup({ open, onClose}) {
+        const handleClose = () => {
+            onClose(); 
+        };
+        return (
+            <Dialog open={open} onClose={onClose} maxWidth={"1000px"} PaperProps={{sx: { borderRadius: "15px"}}}>
+                <DialogTitle>Congratulations!</DialogTitle>
+                <DialogContent>
+                    <Typography>Your profile has been successfully submitted.</Typography>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleApplyMore}>Apply More</Button>
+                    <Button onClick={handleToDashboard}>Go to Dashboard</Button>
+                </DialogActions>
+            </Dialog>
+        );
+    }
+
+    // open job listing popup
+    const openJobListingPopup = (key) => {
+        setCurrentPop(key);
+        setOpenPop(true); 
+        console.log(currentPop);
+    };
+
     // close popups
     const handleApplyMore = () => {
         setOpenCongratsPopup(false); 
@@ -396,15 +418,16 @@ export function JobBoard() {
     };
 
     // toggle save job
-    const toggleSaveJob = (key) => {
+    const toggleSaveJob = (jobDetails) => {
         setIsJobSaved(prevState => {
-            const newSavedStatus = !prevState[key];
-            console.log(`Key: ${key} - Saved Status: ${newSavedStatus ? 'Saved' : 'Unsaved'}`);
+            const currentJobs = prevState[0] || [];
+            const updatedJobs = [...currentJobs, jobDetails];
             return {
                 ...prevState,
-                [key]: newSavedStatus
+                0: updatedJobs
             };
         });
+    
         setShowSavedMessage(true);
         setTimeout(() => setShowSavedMessage(false), 1000);
 
@@ -435,6 +458,7 @@ export function JobBoard() {
     };
        
     
+    
     return (
         <div className={`outerCard2 ${openPop ? 'blur-background' : ''}`}>
             <Dialog open={openPop} onClose={closePop} className={`${openSubmitProfile || openCongratsPopup ? 'blur-effect' : ''}`} maxWidth={"1000px"} PaperProps={{sx: { borderRadius: "15px"}}}>
@@ -456,17 +480,14 @@ export function JobBoard() {
                                     {currentPop[0] && currentPop[0].length > 1 && currentPop[0][1]}
                                 </Typography>
                                 <div style={{ display: 'inline-block', position: 'relative' }}>
-
-                                    <IconButton onClick={() => toggleSaveJob(currentPop[0])} style={{ borderRadius: '10px' }}>
-                                        {isJobSaved[currentPop[0]] ? 
+                                    <IconButton onClick={() => toggleSaveJob(currentPop[0][0])} style={{ borderRadius: '10px' }}>
+                                        {isJobSaved[currentPop] ? 
                                             <StarIcon style={{ color: '#A4A4A4' }} /> : 
                                             <StarBorderIcon style={{ color: '#A4A4A4' }} />}
                                     </IconButton>
-                                    {showSavedMessage && (
-                                        <div style={{ position: 'absolute', bottom: '-25px', left: '50%', transform: 'translateX(-50%)', fontSize: '12px', fontFamily: 'Outfit', textAlign: 'center' }}>
-                                            {isJobSaved[currentPop[0]] ? 'Job Saved' : 'Job Unsaved'}
-                                        </div>
-                                    )}
+                                    {showSavedMessage && <div style={{ position: 'absolute', bottom: '-25px', left: '50%', transform: 'translateX(-50%)', fontSize: '12px', fontFamily: 'Outfit', textAlign: 'center' }}>
+                                    {isJobSaved ? 'Job Saved' : 'Job Unsaved'}
+                                    </div>}
                                 </div>
                             </div>
                             <Typography style={{fontFamily: 'Outfit', fontSize:'20px', color:'#141414', fontWeight: '500', paddingLeft:'1%'}}>
