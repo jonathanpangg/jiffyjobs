dff03d26f94f41db1d157d828c9351743443fbfc
“Calvin <Calvinli0824@gmail.com>
2023-12-08 21:56:58 -0500

    added custom alert for when you hire and reject

--- frontend/jiffyjobs/src/components/ViewApplicants.js
+++ frontend/jiffyjobs/src/components/ViewApplicants.js
@@ -6,6 +6,8 @@ import CloseIcon from '@mui/icons-material/Close';
 import { PostedJobDashboard } from './PostedJobDashboard';
 import { ToastContainer, toast } from 'react-toastify';
 import logo from '../images/Logo.png'
+import accept from '../images/Accept.png'
+import reject from '../images/Reject.png'
 
 export function ViewApplicants({children, jobID}) {
     const [applicantData, setApplicantData] = useState([]) 
@@ -55,9 +57,9 @@ export function ViewApplicants({children, jobID}) {
             })
             .then((_) => {
                 toast.dismiss()
-                toast.info('User Accepted!', {
-                    icon: ({theme, type}) =>  <img src={logo} style={{ width: '24px', height: '24px', marginRight: '10px', marginBottom:'6px'}}/>,
-                    progressStyle: {backgroundColor: '#4A4FE4'},
+                toast.success(`Accepted ${email}`, {
+                    icon: ({theme, type}) =>  <img src={accept} style={{ width: '24px', height: '24px', marginRight: '10px', marginBottom:'6px'}}/>,
+                    progressStyle: {backgroundColor: '#66C120'},
                     position: "top-center",
                     autoClose: 5000,
                     hideProgressBar: false,
@@ -88,7 +90,19 @@ export function ViewApplicants({children, jobID}) {
                 return response.json();
             })
             .then((_) => {
-                
+                toast.dismiss()
+                toast.error(`Rejected ${email}`, {
+                    icon: ({theme, type}) =>  <img src={reject} style={{ width: '24px', height: '24px', marginRight: '10px', marginBottom:'6px'}}/>,
+                    progressStyle: {backgroundColor: '#C12020'},
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
             })
             .catch((error) => {
                 console.log(error)
