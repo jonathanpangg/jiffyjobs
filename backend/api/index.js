4d85987c86d33328ac3d1d86a50ce8e7a24b32cb
Annie Liang <aaliang@bu.edu>
2024-02-15 17:34:21 -0500

    change origin link of cors

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,42 +1,35 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
-import cors from "cors"
-import jobRoutes from "../routes/jobRoutes.js"
-import userRoutes from "../routes/userRoutes.js"
-import authRoutes from "../routes/authRoutes.js"
+import cors from "cors";
+import jobRoutes from "../routes/jobRoutes.js";
+import userRoutes from "../routes/userRoutes.js";
+import authRoutes from "../routes/authRoutes.js";
+
 dotenv.config();
 
 // express app
 const app = express();
 
-// app.use(cors({
-//   origin: '*'
-// }));
+// enable CORS 
+app.use(cors({
+  origin: 'https://jiffyjobs-b7c65.web.app'
+}));
 
-// middleware
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
-
 // routes
-app.use('/api/jobs', jobRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/auth', authRoutes)
-
+app.use('/api/jobs', jobRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
 
 // connect to db
 mongoose.connect(process.env.DB)
     .then(() => {
-        // listen for requests
         app.listen(process.env.PORT, () => {
-            console.log('connected to db & listening on port', process.env.PORT)
-        })
+            console.log('Connected to DB & listening on port', process.env.PORT);
+        });
     })
     .catch((error) => {
-        console.log(error)
-    })
+        console.log(error);
+    });
